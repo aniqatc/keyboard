ea0ab213d016e918669ced4fba28dc4db89f01f5
aniqatc <aniqatc97@gmail.com>
2023-01-02 03:15:06 -0500

    F1-F8 Keypress Functionality to Change Themes

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // Initial Theme
 document.querySelector('body').classList.add('theme-one');
 
+// Change Themes on Click or Keydown
 const themes = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
-
 const themeButtons = document.querySelectorAll('.theme-key');
+let themeKeyCode = [];
+themeButtons.forEach(button =>
+	themeKeyCode.push(button.getAttribute('data-key'))
+);
+
 for (let j = 0; j < themeButtons.length; j++) {
 	themeButtons[j].addEventListener('click', () => {
 		for (let i = 0; i < themeButtons.length; i++) {
@@ -12,3 +17,21 @@ for (let j = 0; j < themeButtons.length; j++) {
 		}
 	});
 }
+
+document.addEventListener('keydown', event => {
+	for (let j = 0; j < themeKeyCode.length; j++) {
+		themeButtons.forEach(el => el.classList.remove('active'));
+		themeButtons[j].classList.add('active');
+		if (event.key === themeKeyCode[j]) {
+			for (let i = 0; i < themeButtons.length; i++) {
+				document.body.classList.remove(`theme-${themes[i]}`);
+				document.body.classList.add(`theme-${themes[j]}`);
+			}
+			break;
+		}
+	}
+});
+
+document.addEventListener('keyup', () => {
+	themeButtons.forEach(el => el.classList.remove('active'));
+});
