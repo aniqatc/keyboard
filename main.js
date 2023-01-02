2a13f82feda0e6ff0abe8da7cdc16c92f85cc261
aniqatc <aniqatc97@gmail.com>
2023-01-02 02:28:14 -0500

    Change Theme on Click (F1 - F8)

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// Initial Theme
 document.querySelector('body').classList.add('theme-one');
 
-////////
+const themes = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+
+const themeButtons = document.querySelectorAll('.theme-key');
+for (let j = 0; j < themeButtons.length; j++) {
+	themeButtons[j].addEventListener('click', () => {
+		for (let i = 0; i < themeButtons.length; i++) {
+			document.body.classList.remove(`theme-${themes[i]}`);
+			document.body.classList.add(`theme-${themes[j]}`);
+		}
+	});
+}
